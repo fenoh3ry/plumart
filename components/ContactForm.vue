bea38a833b205e5c815f35123608d9f2287b4297
fenoh3ry <fenohery.fanomezanirina@gmail.com>
2020-08-15 01:42:49 +0300

    contact

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -2,9 +2,9 @@
   <form>
     <!-- Name -->
     <div class="field">
-      <label class="label is-medium">Name</label>
+      <label class="label is-medium">Nom</label>
       <div class="control has-icons-left">
-        <input class="input is-medium" type="text" placeholder="John Smith" />
+        <input class="input is-medium" type="text" placeholder="Ton nom" />
         <span class="icon is-small is-left">
           <font-awesome-icon icon="user" />
         </span>
@@ -17,7 +17,7 @@
         <input
           class="input is-medium"
           type="email"
-          placeholder="you@email.com"
+          placeholder="ton@email.com"
         />
         <span class="icon is-small is-left">
           <font-awesome-icon icon="envelope" />
@@ -26,12 +26,12 @@
     </div>
     <!-- Subject -->
     <div class="field">
-      <label class="label is-medium">Subject</label>
+      <label class="label is-medium">Objet pour lequel tu nous Contacte</label>
       <div class="control has-icons-left">
         <input
           class="input is-medium"
           type="text"
-          placeholder="Brief Summary"
+          placeholder="Bref Sujet"
         />
         <span class="icon is-small is-left">
           <font-awesome-icon icon="briefcase" />
@@ -44,14 +44,14 @@
       <div class="control">
         <textarea
           class="textarea"
-          placeholder="Detailed description of your comment, request, etc"
+          placeholder="Description détaillé de ton commentaire, demande, etc."
           rows="8"
         ></textarea>
       </div>
     </div>
     <div class="control">
       <button class="button is-primary">
-        Contact Us
+        Nous Contacter
       </button>
     </div>
   </form>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="contact-page" class="page-wrapper contact-page content-page">
     <site-hero
-      title="Contact Us"
-      subtitle="Example Contact Page"
+      title="Nous Contacter"
+      subtitle=""
       image="/uploads/contact-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
@@ -11,10 +11,10 @@
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
-                Hello!
+                Salut!
               </p>
               <p class="subtitle">
-                What can I do for you?
+                Tout d'abord laisser nous vous présenter Plum'Art.
               </p>
               <figure class="image is-1by1 ">
                 <opti-image
@@ -28,15 +28,14 @@
               <div class="content">
                 <p>
                   <strong>
-                    Edit this page in<code>/pages/contact.vue</code>to fit your
-                    needs.
+                   Plum'Art est une association de jeunes artistes Malagasy(15~27ans).
                   </strong>
                 </p>
                 <p>
-                  This form doesn't actually work but would be easy enough to
-                  slap an action on to have it post wherever you'd like. Or put
-                  the netlify attribute on it and have netlify handle all your
-                  form submissions...
+                  Les membres sont tous des jeunes qui veulent évoluer dans le domaine de l'Art et ont  tous des styles de dessin différents: portrait, peinture, manga, BD, illustration ( tradi ou digi).......
+L'association a été fondée en 2014 (association a but non lucratif) et compte plus d'une cinquantaine de membres a ce jour . Nous siégeons actuellement au Global Hub Network Antsahavola.
+Le slogan de l'association est " Évoquons l'artiste qu'on est " 
+Parmi nos objectifs, nous voulons promouvoir les jeunes artistes malagasy. Je veux parler là des jeunes qui n'en fait pas un travail mais un divertissement . Cette promotion se présente sous forme d'exposition, d'atelier de dessin, atelier de rencontre, échange et partage  ...
                 </p>
               </div>
             </article>
